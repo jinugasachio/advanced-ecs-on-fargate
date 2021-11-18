3fbbe96b97e3d31b3a51032fad32c43fe3812224
jinugasachio <jinugasachio@gmail.com>
2021-11-18 23:40:41 +0900

    スクリプト更新

--- scripts/plan.sh
+++ scripts/plan.sh
@@ -1,8 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
-terraform init -input=false -no-color
-terraform plan -input=false -no-color | \
-tfnotify --config ${CODEBUILD_SRC_DIR}/tfnotify.yml plan # --message "$(date)"
+CONFIG_FILE="provider.tf"
+CHANGED_DIRS=$(git --no-pager diff origin/main..HEAD --name-only  | xargs -I{} dirname {} | awk '!a[$0]++{print}') # 差分のあるファイルのディレクトリを一意に取得する
+
+for dir in $CHANGED_DIRS
+do
+  cd $dir
+  if [ -e ${CONFIG_FILE} ]; then # provider.yfが存在するならば
+    terraform init -input=false -no-color
+    terraform plan -input=false -no-color | tfnotify --config ${CODEBUILD_SRC_DIR}/tfnotify.yml plan --message "$dir"
+  fi
+done
 
 # ---------------------------------------------------------------
 # -input=false 実行時の入力を抑制し、未定義の変数がある場合はエラーにする
