50d1615151763f14a29cadbb0caa1351a8d16132
jinugasachio <jinugasachio@gmail.com>
2021-11-28 23:15:37 +0900

    ファイル名修正

--- scripts/apply.sh
+++ scripts/apply.sh
@@ -4,7 +4,7 @@ MESSAGE=$(git log ${CODEBUILD_SOURCE_VERSION} -1 --pretty=format:"%s")
 CODEBUILD_SOURCE_VERSION=$(echo ${MESSAGE} | cut -f4 -d' ' | sed 's/#/pr\//')
 terraform init -input=false -no-color
 terraform apply -input=false -no-color -auto-approve | \
-tfnotify --config ${CODEBUILD_SRC_DIR}/tfnotify.yml apply # --message "$(date)"
+tfnotify --config ${CODEBUILD_SRC_DIR}/tfnotify.yaml apply # --message "$(date)"
 
 # ---------------------------------------------------------------------------------
 # -input=false  実行時の入力を抑制し、未定義の変数がある場合はエラーにする
