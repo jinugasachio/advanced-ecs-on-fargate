d2414670d293773fe030895c4e85304ec69f6f7b
jinugasachio <jinugasachio@gmail.com>
2021-11-28 21:20:53 +0900

    fix

--- scripts/plan.sh
+++ scripts/plan.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 CONFIG_FILE="provider.tf"
 CHANGED_DIRS=$(git --no-pager diff origin/main..HEAD --name-only  | xargs -I{} dirname {} | awk '!a[$0]++{print}') # 差分のあるファイルのディレクトリを一意に取得する
