62d32f09bcc5e2733555c7a6879cd448e6361494
Yukio Ugajin <jinugasachio@gmail.com>
2021-12-11 00:41:07 +0900

    t

--- scripts/github_action/detect_target_dirs.sh
+++ scripts/github_action/detect_target_dirs.sh
@@ -4,9 +4,11 @@ CONFIG_FILE="provider.tf"
 CHANGED_DIRS=`./scripts/github_action/detect_changed_dirs.sh`
 TARGET_DIRS=()
 
+echo ${CHANGED_DIRS[@]}
+
 for dir in $CHANGED_DIRS
 do
-  cd ${GITHUB_WORKSPACE}/$dir
+  cd ./$dir
   if [ -e ${CONFIG_FILE} ]; then # provider.tfが存在するならば
     TARGET_DIRS+=($dir)
   fi
