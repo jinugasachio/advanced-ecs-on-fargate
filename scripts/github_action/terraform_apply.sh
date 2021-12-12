36072c8a936d58e12cbce5c48ae49fa19ddfb047
Yukio Ugajin <jinugasachio@gmail.com>
2021-12-12 18:01:20 +0900

    スクリプトファイルを分けたので削除

--- scripts/github_action/terraform_apply.sh
+++ scripts/github_action/terraform_apply.sh
@@ -7,8 +7,6 @@ for dir in $TARGET_DIRS
 do
   cd ${GITHUB_WORKSPACE}/$dir
   if [ -e ${CONFIG_FILE} ]; then # provider.tfが存在するならば
-    terraform init -input=false
-    terraform validate
     tfcmt --config ${GITHUB_WORKSPACE}/tfcmt.yaml -var target:$dir apply -- terraform apply -auto-approve
   fi
 done
