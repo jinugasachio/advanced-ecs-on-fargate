4e64e97e342a8d12e7a593ce656f22ec35bef87a
Yukio Ugajin <jinugasachio@gmail.com>
2021-12-12 19:40:53 +0900

    スクリプト共通化のために分割

--- scripts/github_action/add_darwin_amd64.sh
+++ scripts/github_action/add_darwin_amd64.sh
@@ -6,10 +6,7 @@ TARGET_DIRS=$@
 for dir in $TARGET_DIRS
 do
   cd ${GITHUB_WORKSPACE}/$dir
-  if [ -e ${CONFIG_FILE} ]; then # provider.tfが存在するならば
-    terraform init -upgrade -input=false
+  if [ -e ${CONFIG_FILE} ]; then
     terraform providers lock -platform=darwin_amd64 -platform=linux_amd64
-    terraform validate
-    tfcmt --config ${GITHUB_WORKSPACE}/tfcmt.yaml -var target:$dir plan -- terraform plan
   fi
 done
