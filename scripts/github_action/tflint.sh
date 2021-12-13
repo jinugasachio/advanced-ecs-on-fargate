d1e1beeef078483fb563eb3372858cc1f12fbc41
Yukio Ugajin <jinugasachio@gmail.com>
2021-12-13 19:23:51 +0900

    スクリプト修正

--- scripts/github_action/tflint.sh
+++ scripts/github_action/tflint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 TARGET_DIRS=$@
-echo $TARGET_DIRS | xargs -r -n 1 bash -c 'cd ${GITHUB_WORKSPACE}/${0} && tflint --config ${GITHUB_WORKSPACE}/.tflint.hcl'
+echo $TARGET_DIRS | xargs -r -n 1 bash -c 'cd ${GITHUB_WORKSPACE}/${0} && tflint --config ${GITHUB_WORKSPACE}/.tflint.hcl --format compact'
