83426df254bee4aa4e1c405d017c1b8295a23c42
Yukio Ugajin <jinugasachio@gmail.com>
2022-01-10 17:19:48 +0900

    ワークフロー修正

--- scripts/github_action/terraform_plan.sh
+++ scripts/github_action/terraform_plan.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-dir=$(pwd)
-dir=${dir##$GITHUB_WORKSPACE/environments/}
-tfcmt --config $GITHUB_WORKSPACE/tfcmt.yaml -var target:$dir plan -- terraform plan \
-  && terraform --version # terragruntの仕様故に最後にversionを出力する必要がある。https://github.com/gruntwork-io/terragrunt/blob/f33fe8c6e1941e79ef99b93b01bbb23f80531a71/cli/version_check.go#L94 
+set -euo pipefail
+
+TARGET_DIRS=$@
+echo $TARGET_DIRS | xargs -r -n 1 bash -c 'cd ${GITHUB_WORKSPACE}/${0} && tfcmt --config ${GITHUB_WORKSPACE}/tfcmt.yaml -var target:${0} plan -- terragrunt plan'
