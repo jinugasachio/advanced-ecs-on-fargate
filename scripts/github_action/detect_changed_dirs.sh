9b8fff3cda1705c590a79fea35aacc3dc389b875
Yukio Ugajin <jinugasachio@gmail.com>
2022-01-10 17:55:06 +0900

    微修正

--- scripts/github_action/detect_changed_dirs.sh
+++ scripts/github_action/detect_changed_dirs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# 特定のディレクトリ内の差分のあるファイルのディレクトリを一意に取得する
-RELATIVE_DIR=$1
-git --no-pager diff origin/main..HEAD --name-only --relative=$RELATIVE_DIR | xargs -I{} dirname {} | awk '!a[$0]++{print}'
+# 指定ディレクトリ内の差分のあるファイルのディレクトリを一意に取得する
+SPECIFIED_DIR=$1
+git --no-pager diff origin/main..HEAD --name-only $SPECIFIED_DIR | xargs -I{} dirname {} | awk '!a[$0]++{print}'
--- scripts/github_action/terraform_plan.sh
+++ scripts/github_action/terraform_plan.sh
@@ -2,5 +2,5 @@
 
 set -euo pipefail
 
-TARGET_DIRS=$@
-echo $TARGET_DIRS | xargs -r -n 1 bash -c 'cd ${GITHUB_WORKSPACE}/${0} && tfcmt --config ${GITHUB_WORKSPACE}/tfcmt.yaml -var target:${0} plan -- terragrunt plan'
+CHANGED_DIRS=$@
+echo $CHANGED_DIRS | xargs -r -n 1 bash -c 'cd ${GITHUB_WORKSPACE}/${0} && tfcmt --config ${GITHUB_WORKSPACE}/tfcmt.yaml -var target:${0} plan -- terragrunt plan'
