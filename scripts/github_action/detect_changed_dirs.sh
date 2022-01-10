f2033ccdad786640271d312cf3eae63873bd12fc
Yukio Ugajin <jinugasachio@gmail.com>
2022-01-10 19:53:14 +0900

    差分スクリプトは直接書く

--- scripts/github_action/detect_changed_dirs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# 指定ディレクトリ内の差分のあるファイルのディレクトリを一意に取得する
-SPECIFIED_DIR=$1
-git --no-pager diff origin/main..HEAD --name-only $SPECIFIED_DIR | xargs -I{} dirname {} | awk '!a[$0]++{print}'
