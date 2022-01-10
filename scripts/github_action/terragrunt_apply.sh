7f512b1c67e854750eb8cd27abfeb899461ed408
jinugasachio <jinugasachio@gmail.com>
2022-01-10 18:46:18 +0900

    auto-approveフラグ追加

--- scripts/github_action/terragrunt_apply.sh
+++ scripts/github_action/terragrunt_apply.sh
@@ -3,4 +3,4 @@
 set -euo pipefail
 
 CHANGED_DIRS=$@
-echo $CHANGED_DIRS | xargs -r -n 1 bash -c 'cd ${GITHUB_WORKSPACE}/${0} && tfcmt --config ${GITHUB_WORKSPACE}/tfcmt.yaml -var target:${0} apply -- terragrunt apply'
+echo $CHANGED_DIRS | xargs -r -n 1 bash -c 'cd ${GITHUB_WORKSPACE}/${0} && tfcmt --config ${GITHUB_WORKSPACE}/tfcmt.yaml -var target:${0} apply -- terragrunt apply -auto-approve'
